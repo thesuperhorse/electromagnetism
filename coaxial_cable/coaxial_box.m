acc5d092e0cc17c61d9e9b58e04683de80b4eeba
thesuperhorse <gallop.fan@mail.utoronto.ca>
2017-10-31 15:15:59 -0400

    Delete coaxial_box.m

--- coaxial_cable/coaxial_box.m
+++
@@ -1,8 +0,0 @@
-function [ V, Ex, Ey ] = coaxial_box(a, b, c, d, x0, y0, V0)
-%[V, Ex, Ey] = coaxial_box(a, b, c, d, x0, y0, V0)
-%   calculates voltage using iterative applications of the laplacian
-Nx = 101;
-Ny = 101;
-V = zeros(Nx, Ny);
-end
-
